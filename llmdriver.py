15803fc08de379e6b614a8bc0283ad8e02bf43f1
downthecrop <bryce@mathx.ca>
2025-06-10 08:27:35 -0700

    Revert message history reduction (only required for local)

--- llmdriver.py
+++ llmdriver.py
@@ -36,7 +36,7 @@ if(IS_LOCAL):
 else:
     STREAM_TIMEOUT = 60
 
-CLEANUP_WINDOW = 5
+CLEANUP_WINDOW = 10 # Sometimes 4 is a good choice for local
 
 SCREENSHOT_PATH = "latest.png"
 MINIMAP_PATH = "minimap.png"
