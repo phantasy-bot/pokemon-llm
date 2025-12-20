89894e4980130fc99aa0df630c9996a3eec22689
Lite <lightwithoutlimit@proton.me>
2025-12-19 21:27:28 -0500

    Migrate to GitLab: remove llmletsplay, replace pokemon-ui with livestream submodule

--- llmletsplay/.vite/deps/react.js
+++
@@ -1,4 +0,0 @@
-import {
-  require_react
-} from "./chunk-MHXMSILQ.js";
-export default require_react();
--- llmletsplay/src/components/sections/LLMPrompts.tsx
+++
@@ -1,5 +0,0 @@
-import { ComingSoon } from './ComingSoon'
-
-export function LLMPrompts() {
-  return <ComingSoon title="Prompts" />
-}
